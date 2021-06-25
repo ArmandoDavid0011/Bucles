9a2986cf237982cf0e8e0718e88070bfbf171ba3
Armando David Camacho Merino <83479028+ArmandoDavid0011@users.noreply.github.com>
2021-06-25 00:39:30 -0500

    Update while.py

--- while.py
+++ while.py
@@ -3,6 +3,6 @@ import math
 numero=int(input("Digite un valor positivo: "))
 
 while numero<0:
-    print("No sea pendejo, tiene que ser numero positivo: ")
-    numero=int(input("Digite un valor posito: "))
+    print("Tiene que ser numero positivo: ")
+    numero=int(input("Digite un valor positivo: "))
 print(f"El resultado de la Raiz cuadrada es: {math.sqrt(numero):.2f}")
